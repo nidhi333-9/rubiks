28b8b59ac6827528d61b585a46be23d5f0e7597a
techynidhi <nidhi129kumari@gmail.com>
2025-01-13 18:12:34 +0530

    updated the js file

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,14 @@
-
-// const menuToggle = document.getElementById('menu-toggle');
-// const menu = document.getElementById('menu');
-
-
-// menuToggle.addEventListener('click', () => {
-//   menu.classList.toggle('hidden');
-// });
 const menuToggle = document.getElementById('menu-toggle');
 const menu = document.getElementById('menu');
 
 menuToggle.addEventListener('click', () => {
-  menu.classList.toggle('hidden'); // Toggle the hidden class
+  menu.classList.toggle('hidden'); 
 });
 
-// Close the menu after clicking a link
+
 const menuLinks = menu.querySelectorAll('a');
 menuLinks.forEach((link) => {
   link.addEventListener('click', () => {
-    menu.classList.add('hidden'); // Close menu on click
+    menu.classList.add('hidden'); 
   });
 });
